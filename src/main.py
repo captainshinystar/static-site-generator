161d3643a1cb31f601ea011f3988e1b42c12d4b7
captainshinystar <joshie.dover@gmail.com>
2025-03-25 23:10:56 -0500

    add generated site for Github Pages

--- src/main.py
+++ src/main.py
@@ -1,22 +1,27 @@
 import os
 import shutil
+import sys
 from website_functions import copy_static, generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "template.html"
+basepath = "/"
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
 
 def main():
-    print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    print("Deleting docs directory...")
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
-    print("Copying static files to public directory...")
-    copy_static(dir_path_static, dir_path_public)
+    print("Copying static files to docs directory...")
+    copy_static(dir_path_static, dir_path_docs)
     
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
+
 
 if __name__ == "__main__":
     main()
--- src/website_functions.py
+++ src/website_functions.py
@@ -24,7 +24,7 @@ def extract_title(md):
             return line[2:]
     raise ValueError("No title found")
     
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         contents = f.read()
@@ -33,17 +33,17 @@ def generate_page(from_path, template_path, dest_path):
     html_node = markdown_to_html_node(contents)
     html = html_node.to_html()
     title = extract_title(contents)
-    updated_template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    output = template.replace("{{ Title }}", title).replace("{{ Content }}", html).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w') as f:
-        f.write(updated_template)
+        f.write(output)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
-        if os.path.isfile(from_path):
+        if os.path.isdir(from_path):
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+        elif filename.endswith('.md'):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
-        else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
